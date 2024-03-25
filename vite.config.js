6df8eb611105e2275f450219de324f2a35450bd8
Hiroshi <soumitm97@gmail.com>
2024-03-25 10:48:17 +0530

    Base Url Added

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/my-portfolio-react-website/',
   build:{
     outDir:"./docs"
   }
